922be37c22dfafd205d34531bea4d2024c5af22d
Eren Gazioglu <eren.g94@gmail.com>
2026-06-02 17:26:22 +0200

    start using cout cerr

--- cpp-00/ex00/megaphone.cpp
+++ cpp-00/ex00/megaphone.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
 
-int main() {
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cerr \
+            << "Error: Wrong number of arguments.\n" \
+            << "Usage: ./megaphone.out <string_to_print>\n";
+        return 1;
+    }
+    std::string msg = argv[1];
     std::cout << "Hello World!\n";
     return 0;
 }
