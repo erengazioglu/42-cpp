15bfb1b645babec8c27f0152f980e87ce54faf36
Eren Gazioglu <eren.g94@gmail.com>
2026-06-25 18:21:14 +0200

    solve ex00

--- cpp-00/ex00/megaphone.cpp
+++ cpp-00/ex00/megaphone.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
 #include <string>
 
-void megaphone(std::string msg) {
-    for (int i = 0; i < (int) msg.length(); i++) {
-        // trim left and right
-        if (std::isalpha(msg[i]))
-            msg[i] = std::toupper(msg[i]);
-    }
-    std::cout << msg;
-}
-
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cerr \
-            << "Error: Wrong number of arguments.\n" \
-            << "Usage: ./megaphone.out <string_to_print>\n";
-        return 1;
+    if (argc < 2) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return 0;
     }
-    std::string msg = argv[1];
-    for (int i = 0; i < (int) msg.length(); i++) {
-        if (std::isalpha(msg[i]))
-            msg[i] = std::toupper(msg[i]);
+    for (int i = 1; i < argc; i++) {
+        std::string msg = argv[i];
+        for (int c = 0; c < (int) msg.length(); c++) {
+            if (std::isalpha(msg[c]))
+                msg[c] = std::toupper(msg[c]);
+        }
+        std::cout << msg;
     }
-    std::cout << msg << std::endl;
+    std::cout << std::endl;
     return 0;
 }
