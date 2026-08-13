9504aebadfa0516d7aa26285bb5db9cb2f4269da
Eren <eren.g94@gmail.com>
2026-08-14 01:44:13 +0200

    test static member variable

--- tests/00-accounts/main.cpp
+++ tests/00-accounts/main.cpp
@@ -5,14 +5,19 @@
 
 int main(void) {
 	Account acc1;
+	Account acc2;
 
 	acc1.deposit(100);
+	acc2.deposit(33);
 	std::cout << acc1.getAmount() << "\n";
 	std::cout << acc1.deposit(50) << "\n";
-	
+	std::cout << "Total: " << Account::getTotalAmount() << "\n";
+
+
 	std::cout << "withdrawing 20: got " << acc1.withdraw(20);
 	std::cout << ", left with " << acc1.getAmount() << "\n";
 	
 	std::cout << "withdrawing 150: got " << acc1.withdraw(150);
 	std::cout << ", left with " << acc1.getAmount() << "\n";
+	
 }
