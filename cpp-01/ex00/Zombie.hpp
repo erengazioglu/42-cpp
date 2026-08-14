071ccec3c255ab37f5883718e2799ccdf0a5c750
Eren Gazioglu <eren.g94@gmail.com>
2026-08-14 19:08:30 +0200

    add destructor

--- cpp-01/ex00/Zombie.hpp
+++ cpp-01/ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/23 13:44:09 by egaziogl          #+#    #+#             */
-/*   Updated: 2026/07/23 14:00:45 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/08/14 19:06:13 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,21 @@
 # include <string>
 # include <iostream>
 
+# define RED "\033[31m"
+# define GRN "\033[32m"
+# define YEL "\033[33m"
+# define BLU "\033[34m"
+# define MAG "\033[35m"
+# define CYN "\033[36m"
+# define RST "\033[0m"
+
 class Zombie {
 	public:
 		Zombie();
+		~Zombie();
 		void	announce(void);
-		void	set_name(std::string name);
 		std::string	get_name(void);
+		void		set_name(std::string name);
 
 	private:
 		std::string _name;
