7b358802fe81ae82a0303e0031cbdb212d163f3c
Eren Gazioglu <eren.g94@gmail.com>
2026-08-16 02:57:57 +0200

    start working on ex04

---
+++ cpp-01/ex04/sedpp.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sedpp.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/16 02:54:26 by egaziogl          #+#    #+#             */
+/*   Updated: 2026/08/16 02:55:55 by egaziogl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SEDPP_H
+# define SEDPP_H
+
+# define RED "\033[31m"
+# define GRN "\033[32m"
+# define YEL "\033[33m"
+# define BLU "\033[34m"
+# define MAG "\033[35m"
+# define CYN "\033[36m"
+# define RST "\033[0m"
+
+# include <fstream>
+# include <iostream>
+
+#endif
