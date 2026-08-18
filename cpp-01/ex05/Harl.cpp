d945fed20c29bb5355d422324c1059b13246def9
Eren Gazioglu <eren.g94@gmail.com>
2026-08-18 18:30:51 +0200

    Solve harl

--- cpp-01/ex05/Harl.cpp
+++ cpp-01/ex05/Harl.cpp
@@ -6,7 +6,37 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/17 18:56:48 by egaziogl          #+#    #+#             */
-/*   Updated: 2026/08/17 19:02:05 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/08/18 18:29:06 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+#include <iostream>
+
+void Harl::debug(void) {
+	std::cout << "Debug message\n";
+}
+
+void Harl::info(void) {
+	std::cout << "Info message\n";
+}
+
+void Harl::warning(void) {
+	std::cout << "Warning message\n";
+}
+
+void Harl::error(void) {
+	std::cout << "Error message\n";
+}
+
+void Harl::complain(std::string level) {
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void (Harl::*funcs[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	for (int i = 0; i < 4; i++) {
+		if (levels[i] == level) {
+			(this->*funcs[i])();
+			return;
+		}
+	}
+	std::cout << "Default message\n";
+}
--- cpp-01/ex05/main.cpp
+++ cpp-01/ex05/main.cpp
@@ -6,12 +6,20 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/17 18:57:34 by egaziogl          #+#    #+#             */
-/*   Updated: 2026/08/17 18:58:03 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/08/18 18:30:24 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+#include <iostream>
 
 int main(int argc, char **argv) {
+	if (argc != 2) {
+		std::cout << RED << "Usage: ./harl <level>\n" << RST;
+		return 1;
+	}
+	Harl harl;
+	harl.complain(argv[1]);
+
 	return 0;
 }
