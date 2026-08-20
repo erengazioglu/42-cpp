47513d29ea424303622247a66ee1aed649ad1312
Eren Gazioglu <eren.g94@gmail.com>
2026-08-20 17:34:34 +0200

    add copy assignment operator

--- cpp-02/ex00/Fixed.hpp
+++ cpp-02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/20 15:05:34 by egaziogl          #+#    #+#             */
-/*   Updated: 2026/08/20 15:33:16 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/08/20 15:49:03 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,11 @@
 class Fixed {
 	public:
 		Fixed();
-		Fixed(Fixed& src);
+		Fixed(const Fixed& src);
 		~Fixed();
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
+		Fixed& operator=(const Fixed& src);
 	private:
 		static const int _fract = 8;
 		int _fixed;
