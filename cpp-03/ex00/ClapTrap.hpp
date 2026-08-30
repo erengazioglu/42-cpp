185d8278285b302d1278101b141501241b885062
Eren Gazioglu <eren.g94@gmail.com>
2026-08-30 23:45:23 +0200

    write and test everything ex00

--- cpp-03/ex00/ClapTrap.hpp
+++ cpp-03/ex00/ClapTrap.hpp
@@ -6,18 +6,28 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/30 23:09:08 by egaziogl          #+#    #+#             */
-/*   Updated: 2026/08/30 23:14:16 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/08/30 23:34:40 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAPTRAP_H
 # define CLAPTRAP_H
 
+# define RED "\033[31m"
+# define GRN "\033[32m"
+# define YEL "\033[33m"
+# define BLU "\033[34m"
+# define MAG "\033[35m"
+# define CYN "\033[36m"
+# define RST "\033[0m"
+
 # include <string>
+# include <iostream>
 
 class ClapTrap {
 	public:
 		ClapTrap();
+		ClapTrap(std::string name);
 		ClapTrap(const ClapTrap&);
 		~ClapTrap();
 		ClapTrap&	operator=(const ClapTrap&);
@@ -26,11 +36,16 @@ class ClapTrap {
 		void takeDamage(unsigned int amount);
 		void beRepaired(unsigned int amount);
 
+		std::string get_name(void)	const;
+		int get_hp(void)			const;
+		int get_ep(void)			const;
+		int get_dmg(void)			const;
+
 	private:
 		std::string _name;
 		int	_hp;
 		int _ep;
-		int _atk;
+		int _dmg;
 };
 
 #endif
--- cpp-03/ex00/main.cpp
+++ cpp-03/ex00/main.cpp
@@ -6,10 +6,19 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/30 23:18:38 by egaziogl          #+#    #+#             */
-/*   Updated: 2026/08/30 23:18:44 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/08/30 23:44:11 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
 int main(void) {
-	
+	ClapTrap	a("Jambo");
+
+	a.attack("Ugali");
+	a.takeDamage(5);
+	a.beRepaired(10);
+	for (int i = 0; i < 9; i++) {
+		a.attack("Ugali");
+	}
 }
