817218526e6b8fc08328baa7afc58eababcdeda5
Eren Gazioglu <eren.g94@gmail.com>
2026-09-03 18:21:14 +0200

    start writing diamond trap

--- cpp-03/ex03/DiamondTrap.cpp
+++ cpp-03/ex03/DiamondTrap.cpp
@@ -6,9 +6,24 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/03 16:00:06 by egaziogl          #+#    #+#             */
-/*   Updated: 2026/09/03 16:02:27 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/09/03 17:03:11 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "DiamondTrap.hpp"
 
+DiamondTrap::DiamondTrap() {
+	
+}
+
+DiamondTrap::DiamondTrap(std::string name) {
+	
+}
+
+DiamondTrap::~DiamondTrap() {
+	
+}
+
+void DiamondTrap::whoAmI() {
+
+}
