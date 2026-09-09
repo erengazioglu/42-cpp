b66b4dbdc9309003bb781620d772032917338c6c
Eren Gazioglu <eren.g94@gmail.com>
2026-09-09 17:24:58 +0200

    add extensive tests for wronganimal

--- cpp-04/ex00/WrongAnimal.cpp
+++ cpp-04/ex00/WrongAnimal.cpp
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/07 23:47:06 by egaziogl          #+#    #+#             */
-/*   Updated: 2026/09/08 10:18:25 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/09/09 17:21:56 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,7 @@
 
 WrongAnimal::WrongAnimal() : _type("animal") {}
 WrongAnimal::~WrongAnimal() {}
-
 WrongAnimal::WrongAnimal(std::string type) : _type(type) {}
-
 WrongAnimal::WrongAnimal(const WrongAnimal& src) : _type(src._type) {}
 
 WrongAnimal& WrongAnimal::operator=(const WrongAnimal& src) {
@@ -25,16 +23,16 @@ WrongAnimal& WrongAnimal::operator=(const WrongAnimal& src) {
 	return *this;
 }
 
-std::string	WrongAnimal::getType(void) {
+std::string	WrongAnimal::getType(void) const {
 	return _type;
 }
 
-void 	WrongAnimal::announceType(void) {
+void 	WrongAnimal::announceType(void) const {
 	std::ostringstream oss;
 	oss << RED << "[" << _type << "]" << RST;
 	std::cout << std::left << std::setw(20) << oss.str();
 }
 
-void	WrongAnimal::makeSound(void) {
+void	WrongAnimal::makeSound(void) const {
 	std::cout << "** this " << _type << " sounds... wrong **\n";
 }
--- cpp-04/ex00/main.cpp
+++ cpp-04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/07 23:47:24 by egaziogl          #+#    #+#             */
-/*   Updated: 2026/09/08 10:18:00 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/09/09 17:24:28 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,16 @@ int main(void) {
 	}
 	{
 		WrongAnimal	animal;
-		WrongCat	cat;
+		WrongAnimal *cat1 = new WrongCat;
+		WrongCat	cat2;
 
 		animal.announceType();
 		animal.makeSound();
-		cat.announceType();
-		cat.makeSound();
+		cat1->announceType();
+		cat1->makeSound();
+		cat2.announceType();
+		cat2.makeSound();
+		delete cat1;
 	}
 	return 0;
 }
