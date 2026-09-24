24163316733ea5d5b459631b82e55f8e34a7d204
Eren Gazioglu <eren.g94@gmail.com>
2026-09-24 13:35:53 +0200

    copy over main from ex01

--- cpp-04/ex02/main.cpp
+++ cpp-04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/07 23:47:24 by egaziogl          #+#    #+#             */
-/*   Updated: 2026/09/09 17:28:01 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/09/24 13:35:41 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@
 int main(void) {
 	{
 		Animal* zoo[10];
+		std::cout << "(creating cats)\n";
 		for (int i = 0; i < 5; i++)
 			zoo[i] = new Cat;
+		std::cout << "(creating dogs)\n";
 		for (int i = 5; i < 10; i++)
 			zoo[i] = new Dog;
 		for (int i = 0; i < 10; i++) {
@@ -25,14 +27,18 @@ int main(void) {
 			delete zoo[i];
 		}
 	}
+	std::cout << "---\n";
 	{
+		std::cout << "(testing different constructors)\n";
 		Cat a;
 		Cat b(a);
 		Cat c = a;
 		Cat d;
 		d = a;
 	}
+	std::cout << "---\n";
 	{
+		std::cout << "(testing brains, deep copying, and memory leaks)\n";
 		Dog* a = new Dog;
 		Dog b;
 		a->setIdea(0, "Chew on the guest's shoes.");
@@ -44,5 +50,6 @@ int main(void) {
 		delete a;
 		b.readThoughts();
 	}
+
 	return 0;
 }
